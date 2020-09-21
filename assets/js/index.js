f338f8940495bb7c7c8bc5fdf11ae7481ebfea96
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-09-21 13:05:16 +0100

    fixed font-issues on footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ const togleBtn = document.getElementById("togle-btn");
 const headerNav = document.getElementById("header-nav");
 const showMenu = () => {
   togleBtn.addEventListener('click', () => {
-    headerNav.classList.toggle('hidden')
+    headerNav.classList.toggle('hidden');
+    togleBtn.classList.toggle('change')
   });
   // console.log(togleBtn);
 }
